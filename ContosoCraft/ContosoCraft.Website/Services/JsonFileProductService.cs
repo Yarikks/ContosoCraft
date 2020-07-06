0d3cccef859ff728a61705229a415327ab5bebd2
Yarikks <jasper18poil@gmail.com>
2020-07-06 21:40:12 +0300

    Create proejct, add data, add service

---
+++ ContosoCraft/ContosoCraft.Website/Services/JsonFileProductService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCraft.Website.Services
+{
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+
+    }
+}
